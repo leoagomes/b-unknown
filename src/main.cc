a823dd88fb61ecf76099f9ca8c6b7163f914c234
Leonardo G. <leoagomes@users.noreply.github.com>
2026-02-28 17:55:01 -0300

    fix segfault

--- src/main.cc
+++ src/main.cc
@@ -7,17 +7,16 @@
 
 #include "state.hh"
 #include "scene/base.hh"
+#include "scene/manager.hh"
 #include "scene/boot.hh"
 
 std::shared_ptr<state::shared_resources> shared_resources;
-std::shared_ptr<scene::base> scene;
+
+scene::manager scene_manager;
 
 void update_draw_frame();
 
 int main() {
-    shared_resources = std::make_shared<state::shared_resources>();
-    scene = std::make_shared<scene::boot>(shared_resources);
-
     const int width = 800;
     const int height = 600;
 
@@ -25,7 +24,9 @@ int main() {
     InitAudioDevice();
     SetTargetFPS(60);
 
-    scene->init();
+    shared_resources = std::make_shared<state::shared_resources>();
+    auto boot = std::make_shared<scene::boot>(shared_resources);
+    scene_manager.push(boot);
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(update_draw_frame, 60, 1);
@@ -41,6 +42,6 @@ int main() {
 }
 
 void update_draw_frame() {
-    scene->update(GetFrameTime());
-    scene->draw();
+    float dt = GetFrameTime();
+    scene_manager.update_and_draw(dt);
 }
